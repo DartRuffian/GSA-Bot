07fe46a9517076a352b343b7c84f91aa8a33cee3
DartRuffian <skulldude327@gmail.com>
2021-09-29 08:46:11 -0500

    Quote Book
    Allows for the saving/removing of quotes.
    
    TBA - Loading quotes when not running a subcommand

---
+++ cogs/quote_book.py
@@ -0,0 +1,88 @@
+""" Quote Book """
+
+# Discord Imports
+import discord
+from discord.ext import commands
+
+# Other Imports
+from json import load, dump
+from os import chdir
+
+class Quote_Book(commands.Cog, name="Quote Book"):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.group (
+        aliases=["qb"],
+        invoke_without_command=True
+    )
+    async def quotebook(self, ctx):
+        pass
+    
+    @quotebook.command(name="save")
+    async def save_quote(self, ctx, author:discord.Member, aliases, *, message):
+        # Convert a string representation of a list to a list
+        aliases = aliases.strip("[]")
+        aliases = aliases.replace("'", "")
+        aliases = aliases.split(",")
+        
+        chdir(f"{self.bot.BASE_DIR}/resources")
+        with open("quotes.json", "r") as f:
+            quotes = load(f)
+        
+        for data in list(quotes.values()):
+            for alias in aliases:
+                if alias in data["aliases"]:
+                    await ctx.send(f"A quote already exists with an alias of `{alias}`, please pick a new alias and try again!")
+                    return
+        
+        quotes[message] = {
+            "author": author.id,
+            "uploaded_by": ctx.author.id,
+            "aliases": aliases
+        }
+        
+        with open("quotes.json", "w") as f:
+            dump(quotes, f, indent=2)
+        chdir(self.bot.BASE_DIR)
+
+        quote_embed = discord.Embed (
+            title="Quote has been saved!",
+            description=f"> {message}\n-{author.mention}",
+            color=self.bot.get_random_color()
+        )
+        #quote_embed.set_thumbnail(url=author.avatar_url)
+        quote_embed.set_footer(text=f"Quote uploaded by: {ctx.author.nick or ctx.author.name}")
+
+        await ctx.send(embed=quote_embed)
+    
+    @quotebook.command(name="delete")
+    async def delete_quote(self, ctx, alias):
+        chdir(f"{self.bot.BASE_DIR}/resources")
+        with open("quotes.json", "r") as f:
+            quotes = load(f)
+        
+        for quote, data in quotes.items():
+            if alias in data["aliases"]:
+                deleted_quote = [quote, await ctx.guild.fetch_member(quotes[quote]["author"])]
+                del quotes[quote]
+                break
+            
+            await ctx.send(f"No quote was found under the alias `{alias}`, double check your spelling and try again!")
+            return
+        
+        with open("quotes.json", "w") as f:
+            dump(quotes, f, indent=2)
+        chdir(self.bot.BASE_DIR)
+
+        delete_quote_embed = discord.Embed (
+            title="Quote has been deleted!",
+            description=f"> {deleted_quote[0]}\n-{deleted_quote[1].nick or deleted_quote[1].name}",
+            color=self.bot.get_random_color()
+        )
+        delete_quote_embed.set_footer(text=f"Quote was deleted by: {ctx.author.nick or ctx.author.name}")
+        await ctx.send(embed=delete_quote_embed)
+
+def setup(bot):
+    bot.add_cog(Quote_Book(bot))
