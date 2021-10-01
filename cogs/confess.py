ebaca42c2670913f2608100361486b58ac935329
DartRuffian <skulldude327@gmail.com>
2021-09-30 20:45:17 -0500

    Anonymous Confessions
    Takes a message in a dm and sends it for the user anonymously, nothing is saved.

---
+++ cogs/confess.py
@@ -0,0 +1,34 @@
+""" Anonymous Confessions """
+
+# Discord Imports
+import discord
+from discord.ext import commands
+
+# Utils
+from utils import Utils
+
+class Confessions(commands.Cog, name="Confessions"):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command (
+        aliases=["ac", "anon_confess"],
+        brief="Makes an anonymous confession",
+        description="This command only works in a direct message with the bot, which takes a confession and sends it for you to keep it anonymous. Nothing is recorded."
+    )
+    async def anonymous_confession(self, ctx, *, message):
+        if not isinstance(ctx.channel, discord.channel.DMChannel):
+            await ctx.message.delete()
+            await ctx.send("This command can only be used in a private message with the bot, and your message has been deleted so that it won't be seen.")
+            return
+        
+        guild = self.bot.get_guild(844325997566099497)
+        confession_channel = guild.get_channel(888545261675245589)
+
+        await confession_channel.send(message)
+        await ctx.author.send("Your confession has been recorded.")
+
+
+def setup(bot):
+    bot.add_cog(Confessions(bot))
