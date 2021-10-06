8c62e4b0a6ad48815fdbbfd9dd91e5e5980c7f8c
DartRuffian <skulldude327@gmail.com>
2021-10-05 20:19:22 -0500

    Total deleted message count + individual message count
    Purge command now shows total messages deleted and how many messages from each user were deleted

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -11,7 +11,7 @@ class Moderation(commands.Cog, name="Mod Only"):
 
 
     @commands.command (
-        aliases=["delete", "del"],
+        aliases=["delete", "del", "cleanup", "clean"],
         brief="Takes a number and deletes that many messages",
         description="Takes an argument `limit` and deletes that many messages from the current channel.",
         hidden=True
@@ -20,12 +20,20 @@ class Moderation(commands.Cog, name="Mod Only"):
     async def purge(self, ctx, limit: int):
         await ctx.message.delete()
         
-        counter = 0
+        message_count = {}
+
         async for message in ctx.channel.history(limit=limit):
-            counter += 1
+            if message.author.name in message_count.keys():
+                message_count[message.author.name] += 1
+            else:
+                message_count[message.author.name] = 1
+
             await message.delete()
         
-        await ctx.send(f"{counter} messages have been deleted.\n(This message will auto-delete in 5 seconds)", delete_after=5)
+        deleted = sum(message_count.values())
+        messages = [f"{deleted} message{' was' if deleted == 1 else 's were'} removed."]
+        messages.extend(f"- **{author}**: {count}" for author, count in message_count.items())
+        await ctx.send("\n".join(messages), delete_after=10)
 
 
 def setup(bot):
