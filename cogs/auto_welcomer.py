72b8a79d2e48d7dca3f9f4ae343f464af0f7284e
DartRuffian <skulldude327@gmail.com>
2021-10-09 21:11:43 -0500

    Updated welcome message

--- cogs/auto_welcomer.py
+++ cogs/auto_welcomer.py
@@ -10,7 +10,7 @@ class Welcomer(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        await member.send("Welcome to the official GSA discord server for Spring Hill! \nTo \"officially\" join the server, write up an intro for yourself in <#844658740451606559> and read the <#844663424679804929> channel! \nAfter doing both of those, hit the green checkmark in the rules channel to be given access to the rest of the server!")
+        await member.send("Welcome to the official GSA discord server for Spring Hill! \nTo \"officially\" join the server, write up an intro for yourself in <#844658740451606559> and read the <#844663424679804929> channel! \nAfter doing both of those, hit the green checkmark in the rules channel to be given access to the rest of the server!\n\nDon't forget to check out <#844690788943396866> as well, where you can pick whatever roles apply to you!")
 
 
 def setup(bot):
